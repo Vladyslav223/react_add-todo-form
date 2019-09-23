e9c853318684b1e2063fe48bfce5e6b24e645b7f
vladyslav223 <vladyslav223@gmail.com>
2019-09-23 09:38:47 +0300

    Updates

---
+++ precache-manifest.c5e69cb7ceb822e9af6e35a1e3e07be1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ee44ce912e83ecb20d6d9d9cee64fb7",
+    "url": "/react_add-todo-form/index.html"
+  },
+  {
+    "revision": "15960cf89b18bf701b54",
+    "url": "/react_add-todo-form/static/css/main.0a47acdd.chunk.css"
+  },
+  {
+    "revision": "abdb8acc62de50087b07",
+    "url": "/react_add-todo-form/static/js/2.067c16ef.chunk.js"
+  },
+  {
+    "revision": "15960cf89b18bf701b54",
+    "url": "/react_add-todo-form/static/js/main.dc8b4d6e.chunk.js"
+  },
+  {
+    "revision": "4953b3d381bec0cda918",
+    "url": "/react_add-todo-form/static/js/runtime~main.9d188fa2.js"
+  }
+]);
